ea46777bbf62d08f5a80fcf687418d04e98f6692
Saba Mchedlishvili <sabamchedlishvili17@gmail.com>
2024-06-30 22:16:00 +0400

    add notFound page

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+export default function NotFound() {
+  return (
+    <div>
+      <h2>Page not found!</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora,
+        similique.
+      </p>
+      <p>
+        Go to the <Link to="/">Homepage</Link>
+      </p>
+    </div>
+  );
+}
